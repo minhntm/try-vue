5fba4aa7897d5cd84540ff967bc7c01b4c92e6ae
Nguyen Tri Minh <minh.nguyentri.95@gmail.com>
2019-07-12 15:29:00 +0900

    create environment variable with dotenv

--- src/main.js
+++ src/main.js
@@ -11,13 +11,13 @@ Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAVOw-skFwHjRCSkKwWhuT_KDZRb4C7x5A',
-  authDomain: 'try-vue-ef2c1.firebaseapp.com',
-  databaseURL: 'https://try-vue-ef2c1.firebaseio.com',
-  projectId: 'try-vue-ef2c1',
-  storageBucket: '',
-  messagingSenderId: '536056890948',
-  appId: '1:536056890948:web:5bc0052aa26b387d'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
